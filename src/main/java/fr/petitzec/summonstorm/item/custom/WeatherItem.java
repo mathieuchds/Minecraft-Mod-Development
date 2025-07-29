1517b2da606731938403f3088efdd4c04feae11d
mathieu_chds <mathieu.chedas@gmail.com>
2025-07-29 12:27:26 +0200

    visual effect for weather staff

--- src/main/java/fr/petitzec/summonstorm/item/custom/WeatherItem.java
+++ src/main/java/fr/petitzec/summonstorm/item/custom/WeatherItem.java
@@ -2,29 +2,48 @@ package fr.petitzec.summonstorm.item.custom;
 
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.UseAnim;
 import net.minecraft.world.level.Level;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.network.chat.Component;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.ChatFormatting;
+import net.minecraft.core.particles.ParticleTypes;
+import org.jetbrains.annotations.NotNull;
 
-public class WeatherItem extends Item {
+public class WeatherItem extends Item{
 
     public WeatherItem(Properties properties) {
-        super(properties);
+        super(properties.stacksTo(1));
     }
 
     @Override
     public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
-        // Seulement côté serveur
-        if (!level.isClientSide && level instanceof ServerLevel serverLevel) {
-            // Déclenche la pluie pendant 6000 ticks (5 minutes)
-            serverLevel.setWeatherParameters(0, 6000, true, false);
-
-            // Message de debug (facultatif)
-            player.sendSystemMessage(net.minecraft.network.chat.Component.literal("Le ciel s'assombrit..."));
-        }
-
-        return InteractionResultHolder.sidedSuccess(player.getItemInHand(hand), level.isClientSide());
+        ItemStack stack = player.getItemInHand(hand);
+        player.startUsingItem(hand);
+        System.out.println(">>> USE triggered");
+        return InteractionResultHolder.consume(stack);
     }
+
+    //@Override
+    public int getUseDuration(ItemStack stack) {
+        return 80; // 4 seconds
+    }
+
+    @Override
+    public UseAnim getUseAnimation(ItemStack stack) {
+        return UseAnim.BOW;
+    }
+
+    @Override
+    public void onUseTick(Level level, LivingEntity entity, ItemStack stack, int remainingUseTicks) {
+        System.out.println(">>> onUseTick: " + remainingUseTicks);
+    }
+
+
 }
---
+++ src/main/java/fr/petitzec/summonstorm/util/ModItemProperties.java
@@ -0,0 +1,30 @@
+package fr.petitzec.summonstorm.util;
+
+import fr.petitzec.summonstorm.item.ModItems;
+import net.minecraft.client.renderer.item.ItemProperties;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Items;
+
+public class ModItemProperties {
+
+    public static void addCustomItemProperties (){
+        makeCustomStaff(ModItems.WEATHER_STAFF.get());
+    }
+
+    private static void makeCustomStaff(Item item) {
+        ItemProperties.register(item, ResourceLocation.withDefaultNamespace("pull"), (p_340951_, p_340952_, p_340953_, p_340954_) -> {
+            if (p_340953_ == null) {
+                return 0.0F;
+            } else {
+                return p_340953_.getUseItem() != p_340951_ ? 0.0F : (float)(p_340951_.getUseDuration(p_340953_) - p_340953_.getUseItemRemainingTicks()) / 20.0F;
+            }
+        });
+
+        ItemProperties.register(
+                item,
+                ResourceLocation.withDefaultNamespace("pulling"),
+                (p_174630_, p_174631_, p_174632_, p_174633_) -> p_174632_ != null && p_174632_.isUsingItem() && p_174632_.getUseItem() == p_174630_ ? 1.0F : 0.0F
+        );
+    }
+}
